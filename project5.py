70535c91a3da0d676dd729337d185dae4539d215
Phucpham0709 <phamthienphuc0709@gmail.com>
2025-07-25 20:45:56 +0700

    Add files via upload

---
+++ project5.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Giới thiệu bản thân')
+st.write('Họ và tên: Phạm Thiên Phúc')
+st.write('Ngày tháng năm sinh: 07/11/2009')
+st.write('Môn học yêu thích: Môn toán')
+st.write('Sở thích: Chơi thể thao, chơi game')
